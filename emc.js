4e5bd0badff1e067c6d80a3330cbcf11ff5c0f3f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-04 17:37:52 -0500

    0.0.23

---
+++ emc.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
+/** @import {EMC} from './ts-refs/trans-render/be/types' */
+/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-a-beacon/types' */;
+
+/**
+ * @type {EMC<any, AP>}
+ */
+export const emc = {
+    base: 'be-fetching',
+    map: {
+        '0.0': {
+            instanceOf: 'Object',
+            mapsTo: '.'
+        }
+    },
+    enhPropKey: 'beFetching',
+    importEnh: async () => {
+        const { BeFetching } = await import('./be-fetching.js');
+        return BeFetching;
+    }
+};
